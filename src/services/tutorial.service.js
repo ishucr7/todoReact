ede5f6d9ba56211bcfc2c8fdf88eca4384e518e5
Mohd Ali Rizwi <mohdalirizwi01@gmail.com>
2020-06-02 18:38:48 +0530

    Added code for user login, signup and jwt header service

--- src/services/tutorial.service.js
+++ src/services/tutorial.service.js
@@ -1,20 +1,17 @@
 import http from "../http-common";
+import authHeader from './auth-header';
 
 class TutorialDataService {
   getAll() {
-    return http.get("/api/user/tasks", 
-    {headers: {"x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNTkxMDUyMDg0LCJleHAiOjE1OTExMzg0ODR9.NodHGPrVl-2MWMWVE1a4MkXgS9VR-Fu9l6OM9dT6nUQ"}});
+    return http.get("/api/user/tasks", { headers: authHeader() });  
   }
 
   get(id) {
-    return http.get(`/api/user/tasks/${id}`,
-    {headers: {"x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNTkxMDUyMDg0LCJleHAiOjE1OTExMzg0ODR9.NodHGPrVl-2MWMWVE1a4MkXgS9VR-Fu9l6OM9dT6nUQ"}});
+    return http.get(`/api/user/tasks/${id}`, { headers: authHeader() });
   }
 
   create(data) {
-    return http.post("/api/user/tasks", data,
-    {headers: {"x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNTkxMDUyMDg0LCJleHAiOjE1OTExMzg0ODR9.NodHGPrVl-2MWMWVE1a4MkXgS9VR-Fu9l6OM9dT6nUQ"}});
-
+    return http.post("/api/user/tasks", data, { headers: authHeader() });
   }
 
   update(id, data) {
