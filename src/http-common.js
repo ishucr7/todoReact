c379261f83be106c5557c017e73b089246e439d4
Mohd Ali Rizwi <mohdalirizwi01@gmail.com>
2020-06-07 05:23:23 +0530

    Updated

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000", //baseURL that depends on REST APIs url that your Server configures.
+  baseURL: "todo-api-52371.herokuapp.com", //baseURL that depends on REST APIs url that your Server configures.
   headers: {
     "Content-type": "application/json"
   }
