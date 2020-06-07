c8ac64872d20d16f060822e053ef570b8bf9514a
Mohd Ali Rizwi <mohdalirizwi01@gmail.com>
2020-06-07 18:14:19 +0530

    Updated routes

--- src/services/comments.service.js
+++ src/services/comments.service.js
@@ -3,20 +3,21 @@ import authHeader from './auth-header';
 
 class CommentDataService {
   getComments(id) {
-    return http.get(`/api/user/comments/${id}`, { headers: authHeader() });  
+    return http.get(`/api/task/${id}/comments`, { headers: authHeader() });  
   }
+  
   createComment(data){
     console.log("Sending create comment request");
-    return http.post(`/api/user/comments/`,data, { headers: authHeader() });  
+    return http.post(`/api/comments/`,data, { headers: authHeader() });  
   }
 
   deleteComment(id){
     console.log("Entered delete function in service");
-    return http.delete(`/api/user/comments/${id}`, { headers: authHeader()});
+    return http.delete(`/api/comments/${id}`, { headers: authHeader()});
   }
 
   editComment(id, data){
-    return http.put(`/api/user/comments/${id}`, data, { headers: authHeader() });
+    return http.put(`/api/comments/${id}`, data, { headers: authHeader() });
   }
 }
 
